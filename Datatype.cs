305a280cdef9379f1b8c71c30423e8a93b53c5f3
nikhil871 <55742637+nikhil871@users.noreply.github.com>
2019-12-10 16:13:41 +0530

    use various datatypes

---
+++ Datatype.cs
@@ -0,0 +1,34 @@
+using System;
+namespace Datatype{
+class Datatype{
+    
+public static void  Main(string[] args){
+sbyte sb =-3;
+short sh = 5;
+int i = 25;
+long l =-2555;
+byte by = 58;
+ushort us = 25;
+uint ui = 35;
+ulong ul = 5566;
+char c ='b';
+float fl = 355f;
+double d = 8.358674532; 
+bool bl =  false;
+Console.WriteLine("sbyte :"+ sb);
+Console.WriteLine("short :"+ sh);
+
+Console.WriteLine("int :"+ i);
+Console.WriteLine("long :"+ l);
+Console.WriteLine("byte :"+ by);
+Console.WriteLine("ushort :"+ us);
+Console.WriteLine("uint:"+ ui);
+Console.WriteLine("ulong :"+ ul);
+Console.WriteLine("char :"+ c);
+Console.WriteLine("float :"+ fl);
+Console.WriteLine("double :"+ d);
+Console.WriteLine("boolean :"+ bl);
+}
+
+}
+}

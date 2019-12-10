39165b0cf9cf18c2ff55597e6a18096a598edac6
nikhil871 <55742637+nikhil871@users.noreply.github.com>
2019-12-10 11:50:24 +0530

    Add files via upload

---
+++ helloword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Assignment
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello numino");
+        }
+    }
+}

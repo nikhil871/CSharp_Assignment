60705beca684d18b9d2f056bd83c161e588b7e9b
nikhil871 <55742637+nikhil871@users.noreply.github.com>
2019-12-11 10:10:04 +0530

    Use all types of operators

---
+++ operators.cs
@@ -0,0 +1,24 @@
+using System;
+namespace Operators{
+public class Ope{
+
+
+public static void Main(string[] args){
+    int a= 1;
+int b  = 2;
+// airthematic operat
+    Console.WriteLine(a+b);
+    Console.WriteLine(a-b);
+    Console.WriteLine(a/b);
+    Console.WriteLine(a%b);
+    
+    // unirary operator
+Console.WriteLine(a++);
+Console.WriteLine(++a);
+Console.WriteLine(a--);
+Console.WriteLine(--a);
+
+}
+
+}
+}

3ecfe9fdfad41195e85fc74bd8665663820dc8cd
nikhil871 <55742637+nikhil871@users.noreply.github.com>
2019-12-13 11:14:48 +0530

    ExtensionMethod

---
+++ ExtensionMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Extension_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+A a = new A();
+a.Mul(5,3);
+a.Div(8,3);
+       
+        }
+    }
+    class A{
+void add(int a, int b){
+Console.WriteLine(a+b);    
+}
+void sub(int a, int b){
+Console.WriteLine(a-b);    
+}
+
+    }
+    static class B{
+public static void Mul(this A i,int a, int b){
+Console.WriteLine(a*b);    
+}
+public static void Div(this A i,int a, int b){
+Console.WriteLine(a/b);    
+}
+
+
+}
+  
+}

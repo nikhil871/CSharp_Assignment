505d803411e707fad8109eef74b16b39ad876a19
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:38:08 +0530

    Abstract class

---
+++ Abstraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Abstraction
+{
+    public abstract class  Processor{
+public abstract void Core();
+public void process(){
+    Console.WriteLine("processing");
+}
+    }
+    public class Laptop : Processor
+    {
+        public override void Core(){
+Console.WriteLine("four cores");
+}
+        static void Main(string[] args)
+        {
+           Processor ps = new Laptop();
+             ps.Core();
+    }
+}
+}

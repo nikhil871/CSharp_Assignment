4cd6e812c2aaa211f87b104596ce757d90cff7f5
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:42:08 +0530

    Inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class A{
+private int id;
+        private string name;
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+
+    
+    
+        
+    }
+    class B : A {
+static void Main(string[] args)
+        {
+        B b = new B();
+        b.setId(10);
+b.setName("Arun");
+Console.WriteLine(b.getId());
+Console.WriteLine(b.getName());
+
+        }
+    }
+}

f0d090b0350d21016895e8461b8122231c2a1bc5
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:41:14 +0530

    HashSet

---
+++ HashSet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HashSet_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> h =new HashSet<int>();
+            h.Add(5);
+            h.Add(2);
+            h.Add(4);
+            h.Add(6);
+            h.Add(7);
+            h.Add(8);
+            HashSet<int> p =new HashSet<int>();
+            p.Add(3);
+            p.Add(15);
+            p.Add(16);
+            p.Add(61);
+            p.Add(7);
+            p.Add(8);
+
+            
+            Console.WriteLine("Union Operation");
+            h.UnionWith(p);
+             foreach(int a in h){
+                Console.WriteLine(a); 
+
+
+            }  
+                Console.WriteLine("Intersection Operation");
+                h.IntersectWith(p);
+                foreach(var a in h){
+                Console.WriteLine(a);
+
+            }
+               Console.WriteLine("Diffrence Operation");
+                h.ExceptWith(p);
+                foreach(var a in h){
+                Console.WriteLine(a);
+
+            }
+            
+
+
+        }
+    }
+}

8e78dab9d8c31f94a65a9fc9aef9abb63d789d59
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:45:48 +0530

    String

---
+++ String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+        string p= Name();
+
+           Console.WriteLine(p[3]);
+            Console.WriteLine(p.Contains("hn "));
+            Console.WriteLine(p.ToUpper());
+            Console.WriteLine(p.Substring(5,1));
+          Console.WriteLine(Math.Max(25,35));
+
+
+        }
+        static string Name(){
+return Console.ReadLine();
+        }
+    }
+}

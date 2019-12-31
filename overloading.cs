c08a10e0101bca6306773c3004c7cbd80ba37aa1
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:43:51 +0530

    MethodOverloading

---
+++ overloading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        
+        public void add(int a, int b){
+            int c = a+b;
+            Console.WriteLine("Sum of"+a+"and"+b+"is"+c);
+        }
+        // method overloading
+          public void add(int a, int b, int c){
+            int d = a+b+c;
+            Console.WriteLine("Sum of"+a+","+c+"and"+b+"is"+c);
+        }
+        static void Main(string[] args)
+        {
+            Program program= new Program();
+            program.add(1,2);
+            program.add(1,2,3);
+        }
+    }
+}

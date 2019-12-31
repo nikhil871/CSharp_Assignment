6170938df1a543f4703c0483943ca731a4d628b5
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:44:50 +0530

    Overriding

---
+++ overrididng.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace overriding
+{
+    class A{
+        public A(){
+            Console.WriteLine("base constructer calling");
+        }
+         public A(int a){
+            Console.WriteLine("base constructer calling "+a+" times");
+        }
+        public virtual void run(){
+            Console.WriteLine("base class running");
+        }
+    }
+    class B : A
+    {
+         public B() : base(5){
+            
+            Console.WriteLine("derived constructer calling");
+        }
+ public override void run(){
+            Console.WriteLine("Derived class running");
+        }
+        static void Main(string[] args)
+        {
+         
+A b = new B(); 
+b.run();
+       }
+    }
+}

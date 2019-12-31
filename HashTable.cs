a84b2a65fa8223e12634f8594c10f04199dd4e67
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:41:39 +0530

    HashTable

---
+++ HashTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+namespace HashTable_Collecitons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Hashtable hashtable= new Hashtable();
+           hashtable.Add(1,2);
+           hashtable.Add(2,2);
+           hashtable.Add(3,2);
+           hashtable.Add(4,2);
+           ArrayList list= new ArrayList(hashtable.Keys);
+           list.Reverse();
+           foreach(int a in list){
+               Console.WriteLine(a+":"+ hashtable[a]);
+           }
+
+        }
+    }
+}

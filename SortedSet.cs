dd8bedeb2ee182876d04977b1f50ed417aab5cb0
Project <55742637+nikhilnile199@users.noreply.github.com>
2019-12-31 10:45:25 +0530

    SortedSet

---
+++ SortedSet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           SortedSet<int> s= new SortedSet<int>();
+           s.Add(25);
+           s.Add(23);
+           s.Add(21);
+           s.Add(26);
+           SortedSet<int> p= new SortedSet<int>();
+           p.Add(25);
+            p.Add(31);
+             p.Add(32);
+              p.Add(33);
+           foreach(int a in s){
+               Console.WriteLine(a);
+
+           }   
+           Console.WriteLine("Union");
+           s.IntersectWith(p);
+            foreach(int a in s){
+               Console.WriteLine(a);
+
+           }   
+           
+        }
+    }
+}
